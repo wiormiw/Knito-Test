3b5462c2c41daa0a8e2513d02e702a4d2b3aaedf
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-06-10 16:44:30 +0700

    chore: Remove commented code in user repository

--- src/users/infrastructure/persistence/relational/repositories/user.repository.ts
+++ src/users/infrastructure/persistence/relational/repositories/user.repository.ts
@@ -9,18 +9,9 @@ import { AppDataSource } from '../../../../../database/data-source';
 
 @Injectable()
 export class UsersRelationalRepository implements UserRepository {
-  // constructor(
-  //   @InjectRepository(UserEntity)
-  //   private readonly usersRepository: Repository<UserEntity>,
-  // ) {}
 
   async create(data: User): Promise<User> {
     const persistenceModel = UserMapper.toPersistence(data);
-    // const newEntity = await this.usersRepository.save(
-    //   this.usersRepository.create(persistenceModel),
-    // );
-
-    // return UserMapper.toDomain(newEntity);
 
     // RAW SQL
     const result = await AppDataSource.query(
@@ -78,28 +69,7 @@ export class UsersRelationalRepository implements UserRepository {
     sortOptions?: SortUserDto[] | null;
     paginationOptions: IPaginationOptions;
   }): Promise<User[]> {
-    // const where: FindOptionsWhere<UserEntity> = {};
-    // if (filterOptions?.roles?.length) {
-    //   where.role = filterOptions.roles.map((role) => ({
-    //     id: Number(role.id),
-    //   }));
-    // }
-
-    // const entities = await this.usersRepository.find({
-    //   skip: (paginationOptions.page - 1) * paginationOptions.limit,
-    //   take: paginationOptions.limit,
-    //   where: where,
-    //   order: sortOptions?.reduce(
-    //     (accumulator, sort) => ({
-    //       ...accumulator,
-    //       [sort.orderBy]: sort.order,
-    //     }),
-    //     {},
-    //   ),
-    // });
-
-    // return entities.map((user) => UserMapper.toDomain(user));
-
+    
     // RAW SQL
     const params: any[] = [];
     const whereClauses: string[] = [];
@@ -141,11 +111,6 @@ export class UsersRelationalRepository implements UserRepository {
   }
 
   async findById(id: User['id']): Promise<NullableType<User>> {
-    // const entity = await this.usersRepository.findOne({
-    //   where: { id: Number(id) },
-    // });
-
-    // return entity ? UserMapper.toDomain(entity) : null;
 
     // RAW SQL
     const rows = await AppDataSource.query(
@@ -165,11 +130,6 @@ export class UsersRelationalRepository implements UserRepository {
   }
 
   async findByIds(ids: User['id'][]): Promise<User[]> {
-    // const entities = await this.usersRepository.find({
-    //   where: { id: In(ids) },
-    // });
-
-    // return entities.map((user) => UserMapper.toDomain(user));
 
     // RAW SQL
     if (!ids.length) return [];
@@ -192,12 +152,6 @@ export class UsersRelationalRepository implements UserRepository {
 
   async findByEmail(email: User['email']): Promise<NullableType<User>> {
     if (!email) return null;
-
-    // const entity = await this.usersRepository.findOne({
-    //   where: { email },
-    // });
-
-    // return entity ? UserMapper.toDomain(entity) : null;
 
     // RAW SQL
     const rows = await AppDataSource.query(
@@ -225,12 +179,6 @@ export class UsersRelationalRepository implements UserRepository {
   }): Promise<NullableType<User>> {
     if (!socialId || !provider) return null;
 
-    // const entity = await this.usersRepository.findOne({
-    //   where: { socialId, provider },
-    // });
-
-    // return entity ? UserMapper.toDomain(entity) : null;
-
     // RAW SQL
     const rows = await AppDataSource.query(
       `
@@ -249,26 +197,12 @@ export class UsersRelationalRepository implements UserRepository {
   }
 
   async update(id: User['id'], payload: Partial<User>): Promise<User> {
-    // const entity = await this.usersRepository.findOne({
-    //   where: { id: Number(id) },
-    // });
-
+    
     const entity = await this.findById(id);
 
     if (!entity) {
       throw new Error('User not found');
     }
-
-    // const updatedEntity = await this.usersRepository.save(
-    //   this.usersRepository.create(
-    //     UserMapper.toPersistence({
-    //       ...UserMapper.toDomain(entity),
-    //       ...payload,
-    //     }),
-    //   ),
-    // );
-
-    // return UserMapper.toDomain(updatedEntity);
 
     // RAW SQL
     const updated = {
@@ -336,7 +270,6 @@ export class UsersRelationalRepository implements UserRepository {
   }
 
   async remove(id: User['id']): Promise<void> {
-    // await this.usersRepository.softDelete(id);
 
     // RAW SQL
     await AppDataSource.query(
